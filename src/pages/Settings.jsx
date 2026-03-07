e6694a8f8e2e12acc8c25a3218525bd49466fdc8
Grant Fountain <gafountain2004@gmail.com>
2026-03-06 22:40:18 -0500

    add dedicated exercises page, removed settings page from nav wheel

--- src/pages/Settings.jsx
+++
@@ -1,9 +0,0 @@
-function Settings() {
-    return (
-        <div className="min-h-screen bg-black text-white flex items-center justify-center">
-            <h1 className="text-2xl font-bold">Settings</h1>
-        </div>
-    );
-}
-
-export default Settings;
